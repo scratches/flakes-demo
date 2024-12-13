c9a63ae214f2a8e2fb4da983c6f430c7a13124b0
Dave Syer <dsyer@vmware.com>
2024-12-13 11:11:20 +0000

    The flake-utils url is defined globally

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "Flakes demo";
   nixConfig.bash-prompt = "[flake]:$(basename $(pwd))> ";
-  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
